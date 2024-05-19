7b88cf54ea9b89484a02fcfb8989edea9cd86ab2
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-18 18:50:12 -0700

    Added '@Lazy' to RoleDtoConverter as I don't need it to be executed yet(I would need it after filling up a form and trying to save it) Get the role information from the DB give it back to the Application/UI

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,19 +2,23 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
@@ -27,7 +31,19 @@ public class RoleServiceImpl implements RoleService {
 
         List<Role> roleList = roleRepository.findAll();
 
-        return roleList;
+        //I have Role entities from DB
+        //I need to convert those Role entities to DTOs
+        //I need to use ModelMapper
+        //I already created a class called RoleMapper and there are two methods for me to make this conversion happen
+        //I inject it, so I can use the method to convert a bunch of roles thorough stream
+
+        return roleList.stream()
+                .map(roleMapper::convertToDto)
+                .collect(Collectors.toList());//if I am calling a method directly from a class I can use "::"
+
+
+
+
     }
 
     @Override
