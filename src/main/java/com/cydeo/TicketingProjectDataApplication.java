3c3f23c4db12993470c75d6ef32e37301fd248a2
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-18 17:53:24 -0700

    Added ModelMapper convert DTO to Entity, created a class and annotate the class @Configuration then created a method inside then annotated @Bean

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,27 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 
-@SpringBootApplication
+@SpringBootApplication //this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
+
+    //ModelMapper mapper = new Model(); tightly couple not a good practice
+    //I am trying to add bean in the container through @Bean annotation as ModelMapper is not my class
+    //Create a class annotated with @Configuration // these are covered by @SpringBootApplication
+    //Write a method which return obj that I try to add in the container
+    //Annotate this method with @Bean
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+
 
 }
