6c20f2dd828557bf87dba4b6565d1cfb63bb67b6
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-18 22:25:37 -0700

    Brought the obj from the DB then converted entity to DTO

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -2,30 +2,33 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
     public List<UserDTO> listAllUsers() {
 
         List<User> userList = userRepository.findAll(Sort.by("firstName"));
-
-
-
-        return null;
+        return userList.stream().
+                map(userMapper::convertToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -35,7 +38,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDTO user) {
-
+        userRepository.save(userMapper.convertToEntity(user));//saving only one obj that's why I don't need to use stream here
     }
 
     @Override
