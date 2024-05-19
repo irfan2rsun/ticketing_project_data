d978616dc67f08cf9af04e03081ae012bf5db127
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-18 23:14:59 -0700

    Delete bottom is now working, added @Transactional annotation for it, delete bottom working with commit and rollback function in the background by the spring

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -45,7 +45,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteByUserName(String username) {
-
+        userRepository.deleteByUserName(username);
     }
 
     @Override
