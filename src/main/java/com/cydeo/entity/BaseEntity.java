887fdfdc91437142fea11091290afd79db90be38
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-25 12:10:16 -0700

    Added Project entity

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,10 +2,7 @@ package com.cydeo.entity;
 
 import lombok.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,15 +11,34 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity {
-//This information are not needed in the UI, but it's needed in the DB for tracking the data
+    //This information are not needed in the UI, but it's needed in the DB for tracking the data
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Boolean isDeleted = false;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
 
+    @PrePersist//Ensure that entities are correctly initialized and validated before being stored in the database
+    private void onPrePersist() {//to initialize data
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId = 1L;
+    }
+
+    @PreUpdate
+//Ensure that any necessary modifications or validations are applied to an entity right before it is updated in the database
+    private void onPreUpdate() {
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 }
