5bf9790b9df145c3576431c97850a72dfa1b0d40
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-26 00:11:32 -0700

    Complete status logic is working now when clicking on Complete on the UI

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -55,12 +55,12 @@ public class ProjectController {
         projectService.delete(projectCode);
         return "redirect:/project/create";
     }
-//
-//    @GetMapping("/complete/{projectCode}")
-//    public String completeProject(@PathVariable("projectCode") String projectCode) {
-//        projectService.complete(projectService.findById(projectCode));
-//        return "redirect:/project/create";
-//    }
+
+    @GetMapping("/complete/{projectCode}")
+    public String completeProject(@PathVariable("projectCode") String projectCode) {
+        projectService.complete(projectCode);
+        return "redirect:/project/create";
+    }
 //
 //    @GetMapping("/update/{projectCode}")
 //    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
