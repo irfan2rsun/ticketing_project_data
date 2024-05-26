08a50422833e34792e11943650ac457baadc0a89
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-26 00:05:49 -0700

    Delete logic is working now when deleting a project

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
 import com.cydeo.repository.ProjectRepository;
@@ -54,6 +55,10 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public void delete(String code) {
+
+        Project project = projectRepository.findByProjectCode(code);
+        project.setIsDeleted(true);
+        projectRepository.save(project);
 
     }
 }
