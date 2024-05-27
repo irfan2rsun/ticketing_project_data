7dbc71580e4b92326c68377577089bd4893784df
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-27 12:43:28 -0700

    Task delete logic is now working

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,13 +21,17 @@ public class TaskServiceImpl implements TaskService {
 
 
     public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+
         this.taskRepository = taskRepository;
         this.taskMapper = taskMapper;
+
     }
 
     @Override
     public List<TaskDTO> listAllTask() {
+
         return taskRepository.findAll().stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
+
     }
 
     @Override
@@ -36,6 +41,7 @@ public class TaskServiceImpl implements TaskService {
         dto.setAssignedDate(LocalDate.now());
         Task task = taskMapper.convertToEntity(dto);
         taskRepository.save(task);
+
     }
 
     @Override
@@ -46,10 +52,20 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void delete(Long id) {
 
+        Optional<Task> foundTask = taskRepository.findById(id);
+
+        if (foundTask.isPresent()) {
+            foundTask.get().setIsDeleted(true);
+            taskRepository.save(foundTask.get());
+        }
+
     }
 
     @Override
     public TaskDTO findById(Long id) {
+
         return null;
+
     }
+
 }
