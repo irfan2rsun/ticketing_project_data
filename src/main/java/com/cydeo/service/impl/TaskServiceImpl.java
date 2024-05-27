8a168af7a7832582ecc17779a98476a08794f5c2
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-27 12:25:22 -0700

    Working on Task Create Page, Page is now working and all tasks are showing

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,25 +1,29 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
     @Override
     public List<TaskDTO> listAllTask() {
-        return null;
+        return taskRepository.findAll().stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
     }
 
     @Override
