73da7cfb9e2facf3bd0ceb71aeae5d1715c9b4bd
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-05-28 22:09:17 -0700

    Working on listAllNoneCompletedByAssignedManager method

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -1,6 +1,7 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
@@ -13,5 +14,7 @@ public interface ProjectService {
     void delete(String code);
     void complete(String projectCode);
     List<ProjectDTO> listAllProjectDetails();
+
+    List<ProjectDTO> listAllNoneCompletedByAssignedManager(UserDTO assignedManager);
 
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.User;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -16,10 +20,14 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final ProjectService projectService;
+    private final TaskService taskService;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, ProjectService projectService, TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @Override
@@ -70,10 +78,13 @@ public class UserServiceImpl implements UserService {
 
         //Go to DB and get the user with username
         User user = userRepository.findByUserName(username);
-        //Change the isDeleted field to true
-        user.setIsDeleted(true);
-        //Save the obj in DB
-        userRepository.save(user);
+
+        if (checkIfUserCanBeDeleted(user)){
+            //Change the isDeleted field to true
+            user.setIsDeleted(true);
+            //Save the obj in DB
+            userRepository.save(user);
+        }
 
     }
 
@@ -86,4 +97,20 @@ public class UserServiceImpl implements UserService {
                 .map(userMapper::convertToDto)
                 .collect(Collectors.toList());
     }
+
+    private boolean checkIfUserCanBeDeleted(User user) {
+
+        switch (user.getRole().getDescription()) {
+            case "Manager":
+                List<ProjectDTO> projectDTOList = projectService.listAllNoneCompletedByAssignedManager(userMapper.convertToDto(user));
+                return projectDTOList.size() == 0;
+            case "Employee":
+                List<TaskDTO> taskDTOList = taskService.listAllNoneCompletedByAssignedManager(userMapper.convertToDto(user));
+                return taskDTOList.size() == 0;
+            default:
+                return true;
+        }
+
+    }
+
 }
